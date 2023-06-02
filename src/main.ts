50761772c01d2d7d8c3f105d00c8276ab85eb2ca
caoqiubin <caoqiubin@yiwei.com>
2023-06-02 16:12:41 +0800

    🦄 refactor(prisma): prisma提取成单一module

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { PrismaService } from './app/prisma.service';
+import { PrismaService } from './prisma/prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
